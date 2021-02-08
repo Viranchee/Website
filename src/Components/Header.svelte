c0119125341a2556801db292500229465a6fd04d
Viranchee Lotia <viranchee@outlook.com>
2021-02-08 23:22:47 +0530

    Update

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -3,7 +3,7 @@ export let name: string;
 </script>
 
 <h1 class="border-red-400 text-5xl text-blue-400 text-center p-4">
-  Hello {name}!, Viranchee loves Magic
+  Hello {name}!, I am Viranchee
 </h1>
 <p class="text-center p-4">
   Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how
