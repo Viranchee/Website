434d1d288252bf1370fb7d86d4684baee3a490b2
Viranchee Lotia <viranchee@outlook.com>
2021-02-09 16:28:53 +0530

    Staging Build, Webpack change

--- workers-site/webpack.config.js
+++ workers-site/webpack.config.js
@@ -1,1 +1,29 @@
-module.exports = { target: "webworker", entry: "workers-site/index.js", mode: "production" }
+// core
+const path = require("path");
+
+// config
+const mode = process.env.NODE_ENV || "production";
+
+module.exports = {
+  context: __dirname,
+  output: {
+    filename: "worker.js",
+    path: path.resolve(__dirname, "..", "dist"),
+  },
+  mode,
+  resolve: {
+    extensions: [".ts", ".js"],
+    plugins: [],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+};
