d17d2b9dce8e9a331297e9f5ae3a77cca9ca0126
Viranchee Lotia <viranchee@outlook.com>
2021-02-09 18:09:40 +0530

    Remove Source Maps

--- rollup.config.js
+++ rollup.config.js
@@ -43,21 +43,21 @@ function serve() {
 export default {
   input: 'src/main.ts',
   output: {
-    sourcemap: true,
+    sourcemap: false,
     format: 'iife',
     name: 'app',
     file: 'public/build/bundle.js'
   },
   plugins: [
     svelte({
-      preprocess: sveltePreprocess({ sourceMap: !production }),
+      preprocess: sveltePreprocess({ sourceMap: false }),
       compilerOptions: {
         // enable run-time checks when not in production
         dev: !production
       },
 
       preprocess: sveltePreprocess({
-        sourceMap: !production,
+        sourceMap: false,
         postcss: {
           plugins: [
             require("tailwindcss"),
@@ -82,7 +82,7 @@ export default {
     }),
     commonjs(),
     typescript({
-      sourceMap: true,
+      sourceMap: false,
       inlineSources: !production,
       removeComments: production,
     }),
